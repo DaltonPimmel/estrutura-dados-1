bc8b868f8124b75d41e67835940eaf537600b39d
Dalton Pimmel <dalton.cco1@gmail.com>
2016-03-13 22:46:18 -0300

    Ajustando função uniao.

--- exec2_alocacao.c
+++ exec2_alocacao.c
@@ -8,12 +8,21 @@
 int *uniao(int *v, int n, int *v1, int n1){
 
     int i = 0;
+    int *v3;
+
+    int vetor[n + n1];
 
     for(i = 0; i < VETOR; i++){
-        printf("%d\n\n", &v[i]);
+        vetor[i] = &v[i];
     }
     for(i = 0; i < VETOR1; i++){
-        printf("%d\n\n", &v1[i]);
+        vetor[i + VETOR1] = &v1[i];
+    }
+
+    v3 = vetor[0];
+
+    for(i = 0; i < VETOR1 + VETOR; i++){
+        printf("%d\n", &v3[i]);
     }
 
     return i;
@@ -25,7 +34,7 @@ int *uniao(int *v, int n, int *v1, int n1){
 int main(){
 
     int *v, *v1, *v2;
-    int n, n1;
+    int n = 3, n1 = 4;
 
     int vetor[3], vetor1[4];
     int i = 0;
@@ -41,7 +50,7 @@ int main(){
     v = vetor[0];
     v1 = vetor1[0];
 
-    v2 = uniao(v, &n, v1, &n1);
+    v2 = uniao(v, n, v1, n1);
 
     return 0;
 }
